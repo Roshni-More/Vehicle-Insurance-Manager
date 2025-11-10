7cc4177c5dc5cbc005aa042069f962e8f120409b
Roshni More <rmore0604@gmail.com>
2025-11-10 14:51:31 +0530

    Initial level Code

---
+++ VehicleInsurance/src/main/java/com/rt/controller/CustomerController.java
@@ -0,0 +1,13 @@
+package com.rt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomerController {
+	@RequestMapping("/add")
+	public String showIndex() {
+		return "Customer/addCustomer";
+	}
+
+}
